e1a2e5cd43c07072f54f45e7b29cfce3c19b5f8e
Unknown <blobzer@gmail.com>
2018-05-03 00:19:16 -0700

    Initial Commit

---
+++ Textual-Pleasure/Textual-Pleasure/MainWindow.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace Textual_Pleasure
+{
+  /// <summary>
+  /// Interaction logic for MainWindow.xaml
+  /// </summary>
+  public partial class MainWindow
+  {
+    public MainWindow()
+    {
+      InitializeComponent();
+    }
+  }
+}
